7c1e1f91517d8442092964500b335a00bf2de9b2
ted81das <44252479+ted81das@users.noreply.github.com>
2023-07-02 23:01:52 -0700

    Create iweb-remove-nag.php

---
+++ iweb-remove-nag.php
@@ -0,0 +1,20 @@
+<?php
+/* 
+
+Plugin Name: iweb remove nag 
+
+Plugin URI: https://tutsplus.com/ 
+
+Description: Plugin to accompany tutsplus guide to creating plugins, registers a post type. 
+
+Version: 1.0 
+
+Author: iweb studios
+
+Author URI: https://rachelmccollin.com/ 
+
+License: GPLv2 or later 
+
+Text Domain: tutsplus 
+
+*/
