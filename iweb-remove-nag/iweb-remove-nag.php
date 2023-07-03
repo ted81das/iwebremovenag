6fd333e42c0d0d63962bfee5fa19990447e50ff7
ted81das <44252479+ted81das@users.noreply.github.com>
2023-07-02 23:10:22 -0700

    Update iweb-remove-nag.php

--- iweb-remove-nag/iweb-remove-nag.php
+++ iweb-remove-nag/iweb-remove-nag.php
@@ -1,1 +1,28 @@
+<?php
+/* 
 
+Plugin Name: iweb remove nag 
+
+Plugin URI: https://tutsplus.com/ 
+
+Description: Plugin to accompany tutsplus guide to creating plugins, registers a post type. 
+
+Version: 1.0 
+
+Author: iweb studios
+
+Author URI: https://rachelmccollin.com/ 
+
+License: GPLv2 or later 
+
+Text Domain: tutsplus 
+
+*/
+
+add_action('admin_enqueue_scripts', 'ds_admin_theme_style');
+add_action('login_enqueue_scripts', 'ds_admin_theme_style');
+function ds_admin_theme_style() {
+	if (!current_user_can( 'manage_options' )) {
+		echo '<style>.update-nag, .updated, .error, .is-dismissible { display: none; }</style>';
+	}
+}
